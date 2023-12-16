1453788b19bab260bd9dc353fcfc0f8694ed8875
Messai Hailemariam <105058702+hmmessai@users.noreply.github.com>
2023-12-16 09:59:29 +0300

    Update HandleApi.js

--- frontend/src/utils/HandleApi.js
+++ frontend/src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = "https://fullstack-todo-app-backend-6xz7.onrender.com";
+const baseUrl = "https://localhost:5000";
 
 const getAllToDo = (setToDo) => {
   axios
